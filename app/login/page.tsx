46edf2bed38ffad9205c8170fb2dddb39167f564
EzeAguilar <ezequielmaximiliano123@gmail.com>
2024-05-21 16:22:13 -0300

    login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,21 @@
-//import React from 'react'
+import React from 'react'
 
-//const UsersPage = async () => {
-  //  interface User {
-    //    id: number
-      //  name: string
-    //}
-    //const response = await fetch('https://jsonplaceholder.typicode.com/users', {cache: "no-store"})
-    //const users: User[] = await response.json()
+const UsersPage = async () => {
+   interface User {
+        id: number
+        name: string
+    }
+    const response = await fetch('https://jsonplaceholder.typicode.com/users', {cache: "no-store"})
+    const users: User[] = await response.json()
 
 
-    //return (
-      //  <>
-        //<div>Log In</div>
-        //{/*Formulario Login*/}
-        //</>
-    //)
-//}
+    return (
+        <>
+        <div>Log In</div>
+        {/*Formulario Login*/}
+        </>
+    )
+}
 
 //export default UsersPage
 /*import LoginForm from '@/app/ui/login-form';
