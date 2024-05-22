d73057aaf63b64351794427aba2ffb845e703104
Lucas Bazan <Lucasbazan28@gmail.com>
2024-05-22 16:09:51 -0300

    Form agregado en home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import AlbumComponent from "./ui/albumsComponent";
+import { signOut } from '@/auth';
 
 export default function Home() {
   return (
@@ -9,6 +10,16 @@ export default function Home() {
     </Head>
 
     <AlbumComponent />
+    <form
+          action={async () => {
+            'use server';
+            await signOut();
+          }}
+        >
+          <button className="flex h-[60px] w-full items-center justify-center gap-2 rounded-md bg-white p-4 text-lg font-medium text-gray-800 hover:bg-gray-200 hover:text-black md:w-auto md:justify-start">
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+      </form>
     </>
   );
 }
