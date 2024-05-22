ff825c833a39bd19e9323cad25bebbde7721337b
EzeAguilar <ezequielmaximiliano123@gmail.com>
2024-05-22 16:47:36 -0300

    a

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from "next/legacy/image";
+import Image from "next/image";
 import './header.css';
 const Logo = () => {
   return (
@@ -8,7 +8,7 @@ const Logo = () => {
             <Image
             src="/images/Timeless_Sounds_Logo.png"
             alt="Timeless Sounds Logo"
-            layout="fill" // Hace que la imagen ocupe todo el espacio disponible en su contenedor
+            fill // Hace que la imagen ocupe todo el espacio disponible en su contenedor
             objectFit="contain" // Mantiene el aspecto original de la imagen
             />
       </div>
