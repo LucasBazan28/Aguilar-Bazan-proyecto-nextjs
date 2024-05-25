1b9a6b0d1f0369394001eab5b702956737155fa1
EzeAguilar <ezequielmaximiliano123@gmail.com>
2024-05-25 09:02:47 -0300

    Imagen sin errores

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -9,8 +9,9 @@ const Logo = () => {
             src="/images/Timeless_Sounds_Logo.png"
             alt="Timeless Sounds Logo"
             fill // Hace que la imagen ocupe todo el espacio disponible en su contenedor
-            sizes="100vw"
+            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 30vw"   //define algunos tamaños para que next la cargue eficientemente
             style={{ objectFit: 'contain' }} // Mantiene el aspecto original de la imagen
+            priority={true}   //cuando la imagen se usa en el header hay que agregar priority
             />
       </div>
   </div>
